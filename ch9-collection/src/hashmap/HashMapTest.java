1622c6063ec320077946386eb6e2cfb5045838b2
yangwuse <yangwuse@mail.ustc.edu.cn>
2021-04-08 21:08:54 +0800

    ok

--- ch9-collection/src/hashmap/HashMapTest.java
+++ ch9-collection/src/hashmap/HashMapTest.java
@@ -6,7 +6,8 @@ import java.util.Map;
 public class HashMapTest {
   public static void main(String[] args) {
 //    nullTest();
-    synTest();
+//    synTest();
+    capacityTest();
   }
 
   public static void nullTest() {
@@ -72,4 +73,42 @@ public class HashMapTest {
 
   }
 
+  public static void capacityTest() {
+    int total   = 3000000;
+    // best cap = total / 0.75 = total * 4/3 = 40000000
+    int bestCap = 4000000;
+    int halfCap = 2000000;
+    Map<Integer, Integer> map1 = new HashMap<>(bestCap);
+    Map<Integer, Integer> map2 = new HashMap<>(halfCap);
+    Map<Integer, Integer> map3 = new HashMap<>();
+
+    Runnable r1 = () -> {
+      long s1 = System.currentTimeMillis();
+      for (int i = 0; i < total; i++)
+        map1.put(i, i);
+      long e1 = System.currentTimeMillis();
+      System.out.println("初始化为最佳capcity: " + (e1 - s1) + "ms");
+    };
+
+    Runnable r2 = () -> {
+      long s2 = System.currentTimeMillis();
+      for (int i = 0; i < total; i++)
+        map2.put(i, i);
+      long e2 = System.currentTimeMillis();
+      System.out.println("初始化一半capacity: " + (e2 - s2) + "ms");
+    };
+
+    Runnable r3 = () -> {
+      long s3 = System.currentTimeMillis();
+      for (int i = 0; i < total; i++)
+        map3.put(i, i);
+      long e3 = System.currentTimeMillis();
+      System.out.println("未初始化capacity: " + (e3 - s3) + "ms");
+    };
+
+    new Thread(r1).start();
+    new Thread(r2).start();
+    new Thread(r3).start();
+
+  }
 }
